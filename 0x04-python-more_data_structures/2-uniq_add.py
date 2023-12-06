c14b306bca3357cd5fb4757e40243c0872e8b23c
Calton Korir <caltonkorir45@gmail.com>
2023-12-06 19:12:15 +0300

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-from functools import reduce
-
-
 def uniq_add(my_list=[]):
-    unique_list = set(my_list)
-    return reduce((lambda x, y:x + y), unique_list)
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
