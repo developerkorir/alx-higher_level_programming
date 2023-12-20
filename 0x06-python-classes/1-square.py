32ca515de6f59780eb064421118a60c13e652c23
developerkorir <caltonkorir45@gmail.com>
2023-12-20 13:06:11 -0800

    Task 1 solution

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py by Korir
+"""Defines a square """
+
+
+class Square:
+    """Representation of a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
+        self.__size = size
